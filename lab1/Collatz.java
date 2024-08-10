901898fccdc6d790b8b279cd30cb5ee88ea84359
KIYI671 <531585769@qq.com>
2024-08-10 13:26:33 +0800

    Lab1 练习

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,21 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author KIYI
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * 考拉兹猜想（Collatz conjecture）
+     * 是指对于每一个正整数，如果它是奇数，则对它乘3再加1，如果它是偶数，则对它除以2，如此循环，最终都能够得到1。
+     * f ( n ) = { n / 2 if ( n mod 2 ) = 0
+     *             3 * n + 1 if ( n mod 2 ) = 1 }
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n /= 2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
 
